8bc57bef913fcc5e2c880935565539477c714099
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-02-16 09:59:41 -0800

    changin up some styles

--- js/main.js
+++ js/main.js
@@ -1,55 +1,55 @@
+(function() {
+  var requestAnimationFrame = window.requestAnimationFrame 
+  	|| window.mozRequestAnimationFrame 
+  	|| window.webkitRequestAnimationFrame 
+  	|| window.msRequestAnimationFrame;
+  window.requestAnimationFrame = requestAnimationFrame;
+})();
 (function($){
 
 	var app = Bowler();
-	app.render("{{{logo}}}<h1>{{title}}</h1>{{{child \"page\"}}}<a href={{href}} class=\"button secondary\">{{action}}</a>");
+	app.render("{{{logo}}}<h1>{{title}}</h1><p class=\"tagline\">{{tagline}}</p>{{{child \"page\"}}}<a href={{href}} class=\"button secondary\">{{action}}</a>");
 	app.bind('.eight');
 	app.extend({
 		"title" : "Bowler.js",
 		"href" : "https://github.com/jacoblwe20/bowler",
 		"action" : "View more on Github.com",
-		"logo" : "<div class=\"logo\"><i class=\"background\"></i><i class=\"highlights\"></i><i class=\"band\"></i><i class=\"band-highlights\"></i></div>"
+		"logo" : "<div class=\"logo\"><i class=\"background\"></i><i class=\"highlights\"></i><i class=\"band\"></i><i class=\"band-highlights\"></i></div>",
+		"tagline" : "Bowler.js is a Model / View framework for the gentleman in you."
 	});
 	var page = app.Model('page');
 	var pageView = app.View('page'); 
 	pageView.render("{{#each contents}}<{{tag}}>{{{content}}}</{{tag}}>{{/each}}");
 
-	page.extend({
-		"contents" : [
-			{
-				"tag" : "p",
-				"content" : "Bowler.js is a Model / View framework for the gentleman in you."
-			},
-			{
-				"tag" : "h4",
-				"content" : "Use it"
-			},
-			{
-				"tag" : "p",
-				"content" : "Bowler is super simple to use. Just include the file then..."
-			},
-			{
-				"tag" : "pre",
-				"content" : "<code>var app = Bowler();\n\
-app.extend({title : \"hello\"});\n\
-app.render('&lt;p&gt;{{title}}&lt;/p&gt;');\n\
-app.bind('body');</code>"
-			},
-			{
-				"tag" : "h4",
-				"content" : "Child Models"
-			},
-			{
-				"tag" : "p",
-				"content" : "Whats the use of only having one model. Bowler supports child models."
-			},
-			{
-				"tag" : "pre",
-				"content" : "<code>var page = app.Model('page', { world: \"universe\" });\n\
-var pageView = app.View('page', \"&lt;b&gt;{{universe}}&lt;/b&gt;\");\n\
-//changing the main models template\n\
-app.render(\"&lt;h1&gt;{{title}}{{{child \"page\"}}}&lt;/h1&gt;\");</code>"
-			},
-		]
+	page.get('json/main.json', function(err, res){
+		console.log(res);
 	});
+
+	var current = 0;
+	var taglines = [
+		"Bowler.js is a Model / View framework for the gentleman in you.",
+		"Bowler.js is a Model / View framework for the BADA55 in you.",
+		"Bowler.js is a Model / View framework for the manly man in you."
+	]
+
+	var Tagline = function(){
+		if(current === taglines.length){
+			current = 0;
+		}else{
+			current += 1;
+		}
+		app.extend({tagline : taglines[current]});
+		window.setTimeout(function(){
+
+			if(window.requestAnimationFrame){
+				window.requestAnimationFrame(Tagline);
+			}else{
+				Tagline();
+			}
+
+		},3000)
+	};
+
+	Tagline();
 	
 }(jQuery))
