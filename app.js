ddb21d23e8715088b44174aa294f56dea794b514
jacoblwe20 <jacoblowe2.0@gmail.com>
2013-02-17 19:15:07 -0800

    upgrading server for running examples

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ var http    = require('http'),
  
 http.createServer(function (request, response) {
  
-    
+    request.query = (function(){
+        var data = request.url.split('?')[1];
+        if(data){
+            data = data.split('&');
+            var obj = {};
+            for(var i = 0; i < data.length; i += 1){
+                var values = data[i].split('=');
+                obj[values[0]] = values[1];
+            }
+            return obj;
+        }
+    }());
+
+    request.url = request.url.replace(request.url.split('?')[1], '');
      
     var filePath = '.' + request.url;
     if (filePath == './')
@@ -13,6 +26,7 @@ http.createServer(function (request, response) {
          
     var extname = path.extname(filePath);
     var contentType = 'text/html';
+    var status;
     switch (extname) {
         case '.js':
             contentType = 'text/javascript';
@@ -20,28 +34,37 @@ http.createServer(function (request, response) {
         case '.css':
             contentType = 'text/css';
             break;
-    }
+    };
+    var done = function(){
+        console.log('\033[30m' + request.method +' http://127.0.0.1' + port + '\033[36m' + request.url + '\033[32m ' + status + '\033[0m');
+    };
      
     fs.exists(filePath, function(exists) {
      
         if (exists) {
             fs.readFile(filePath, function(error, content) {
                 if (error) {
+                    status = 500;
                     response.writeHead(500);
                     response.end();
+                    done();
                 }
                 else {
+                    status = 200;
                     response.writeHead(200, { 'Content-Type': contentType });
                     response.end(content, 'utf-8');
+                    done();
                 }
             });
         }
         else {
+            status = 200;
             response.writeHead(404);
             response.end();
+            done();
         }
     });
      
 }).listen(port);
  
-console.log('Server running at http://127.0.0.1:' + port);
+console.log('\033[30mServer running at \033[36m http://127.0.0.1:' + port + '\033[0m');
